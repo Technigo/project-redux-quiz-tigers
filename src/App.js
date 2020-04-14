35c3ad528fae2a833420974eb5fde05b2d7f2bd2
Olle Alm <Olle@Olles-MacBook-Pro-2.local>
2020-04-14 14:17:03 +0200

    button

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import { quiz } from 'reducers/quiz'
 
 import { CurrentQuestion } from 'components/CurrentQuestion'
 
+//Creates store, tells redux about reducers
 const reducer = combineReducers({
   quiz: quiz.reducer
 })
 
+//Creates store, create store using reducers and recuved state
 const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <Provider store={store}>
+    <Provider store={store}> {/* Gives access to the store*/}
       <CurrentQuestion />
     </Provider>
   )
--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,22 +1,43 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { quiz } from '../reducers/quiz'
 
+//Recives ??? as imput
 export const CurrentQuestion = () => {
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuesionIndex])
+  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
 
-  if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+  // add dispatch 
+  // a way to message the store
+  // sends actions to the store
+  const dispatch = useDispatch()
+
+  // create state for input from form
+  // const [value, setValue] = useState("")
+
+  //handle submit function to dispatch
+
+  const handleClick = event => {
+    // event.preventDefault() 
+    dispatch(quiz.actions.addGoToNextQuestion({}))
+    //  call dispatch function and action
+
+    if (!question) {
+      return <h1>Oh no! I could not find the current question!</h1>
+    }
+
+    return (
+      <div>
+        <h1>Question: {question.questionText}</h1>
+        {question.options.map(option => {
+          return (
+            <button>{option}</button>
+          )
+        })}
+        <button
+          onClick={handleClick()}
+        >Next</button>
+      </div>
+    )
   }
 
-  return (
-    <div>
-      <h1>Question: {question.questionText}</h1>
-      {question.options.map(option => {
-        return (
-        <button>{option}</button>
-        )
-        
-      })}
-    </div>
-  )
 }
