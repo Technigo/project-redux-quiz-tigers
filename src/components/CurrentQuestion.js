46496f18311b59f4feb811f2f9827fcbe848b0ac
Josephine <josephine@Josephines-Air.lan>
2020-04-14 16:24:30 +0200

    added answers state

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,47 +1,56 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { quiz } from '../reducers/quiz'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { quiz } from "../reducers/quiz";
 
 //Recives ??? as imput
 export const CurrentQuestion = () => {
-  const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const question = useSelector(
+    (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
+  );
 
-  // add dispatch 
+  const answer = useSelector((state) =>
+    state.quiz.answers.find((a) => a.questionId === question.id)
+  );
+  console.log(answer); // question could come from the previous selector in the last example
+
+  // add dispatch
   // a way to message the store
   // sends actions to the store
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   // create state for input from form
   // const [value, setValue] = useState("")
 
   //handle submit function to dispatch
   const handleAnswer = (id, index) => {
-    dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }))
-  }
+    dispatch(quiz.actions.submitAnswer({ questionId: id, answerIndex: index }));
+    console.log(question);
+  };
 
-  const handleClick = event => {
-    // event.preventDefault() 
-    dispatch(quiz.actions.goToNextQuestion({}))
+  const handleClick = (event) => {
+    // event.preventDefault()
+    dispatch(quiz.actions.goToNextQuestion({}));
     //  call dispatch function and action
-  }
+  };
   if (!question) {
-    return <h1>Oh no! I could not find the current question!</h1>
+    return <h1>Oh no! I could not find the current question!</h1>;
   }
 
   return (
     <div>
       <h1>Question: {question.questionText}</h1>
+
       {question.options.map((option, index) => {
         return (
           <button
+            // className={`${answer.isCorrect ? "correct" : ""}`}
             onClick={() => handleAnswer(question.id, index)}
-          >{option}{index}</button>
-        )
+          >
+            {option}
+          </button>
+        );
       })}
-      <button
-        onClick={handleClick}
-      >Next</button>
+      <button onClick={handleClick}>Next</button>
     </div>
-  )
-}
-
+  );
+};
--- src/reducers/quiz.js
+++ src/reducers/quiz.js
@@ -1,26 +1,66 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 // Change these to your own questions!
 const questions = [
-  { id: 1, questionText: 'what organization did Hermione start in her 4th year?', options: [' Society for the Promotion of Elfish Welfare', "Dumbledore's army", ' wizards against the dark arts', 'witches for equal rights'], correctAnswerIndex: 0 },
-  { id: 2, questionText: 'What patronus does Luna Lovegood have?', options: ['Lion', 'Stag', ' Hare ', 'Horse'], correctAnswerIndex: 2 },
-  { id: 3, questionText: 'Ever the eccentric, Dumbledore has a scar above his left knee that is a perfect map of what? ', options: ['The Hogwarts Castle', 'Hogsmeade', 'Diagon Alley', 'The London Underground'], correctAnswerIndex: 3 },
-  { id: 4, questionText: 'Who where Harrys parents?', options: [' William and Elizabeth Potter', 'Henry and Maggie Potter', 'James and Lily Potter'], correctAnswerIndex: 2 },
-  { id: 5, questionText: 'What animal can Professor McGonagall transform her Animagi into?', options: ['A phoenix', 'A cat', 'A black dog', 'An owl'], correctAnswerIndex: 1 }
-]
+  {
+    id: 1,
+    questionText: "what organization did Hermione start in her 4th year?",
+    options: [
+      " Society for the Promotion of Elfish Welfare",
+      "Dumbledore's army",
+      " wizards against the dark arts",
+      "witches for equal rights",
+    ],
+    correctAnswerIndex: 0,
+  },
+  {
+    id: 2,
+    questionText: "What patronus does Luna Lovegood have?",
+    options: ["Lion", "Stag", " Hare ", "Horse"],
+    correctAnswerIndex: 2,
+  },
+  {
+    id: 3,
+    questionText:
+      "Ever the eccentric, Dumbledore has a scar above his left knee that is a perfect map of what? ",
+    options: [
+      "The Hogwarts Castle",
+      "Hogsmeade",
+      "Diagon Alley",
+      "The London Underground",
+    ],
+    correctAnswerIndex: 3,
+  },
+  {
+    id: 4,
+    questionText: "Who where Harrys parents?",
+    options: [
+      " William and Elizabeth Potter",
+      "Henry and Maggie Potter",
+      "James and Lily Potter",
+    ],
+    correctAnswerIndex: 2,
+  },
+  {
+    id: 5,
+    questionText:
+      "What animal can Professor McGonagall transform her Animagi into?",
+    options: ["A phoenix", "A cat", "A black dog", "An owl"],
+    correctAnswerIndex: 1,
+  },
+];
 
 const initialState = {
   questions,
   answers: [],
   currentQuestionIndex: 0,
-  quizOver: false
-}
+  quizOver: false,
+};
 
 export const quiz = createSlice({
-  name: 'quiz',
+  name: "quiz",
   initialState,
   reducers: {
-
     /**
      * Use this action when a user selects an answer to the question.
      * The answer will be stored in the `quiz.answers` state with the
@@ -37,15 +77,19 @@ export const quiz = createSlice({
      * and `answerIndex` keys. See the readme for more details.
      */
     submitAnswer: (state, action) => {
-      const { questionId, answerIndex } = action.payload
-      const question = state.questions.find((q) => q.id === questionId)
+      const { questionId, answerIndex } = action.payload;
+      const question = state.questions.find((q) => q.id === questionId);
 
       if (!question) {
-        throw new Error('Could not find question! Check to make sure you are passing the question id correctly.')
+        throw new Error(
+          "Could not find question! Check to make sure you are passing the question id correctly."
+        );
       }
 
       if (question.options[answerIndex] === undefined) {
-        throw new Error(`You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`)
+        throw new Error(
+          `You passed answerIndex ${answerIndex}, but it is not in the possible answers array!`
+        );
       }
 
       state.answers.push({
@@ -53,8 +97,8 @@ export const quiz = createSlice({
         answerIndex,
         question,
         answer: question.options[answerIndex],
-        isCorrect: question.correctAnswerIndex === answerIndex
-      })
+        isCorrect: question.correctAnswerIndex === answerIndex,
+      });
     },
 
     /**
@@ -66,9 +110,9 @@ export const quiz = createSlice({
      */
     goToNextQuestion: (state) => {
       if (state.currentQuestionIndex + 1 === state.questions.length) {
-        state.quizOver = true
+        state.quizOver = true;
       } else {
-        state.currentQuestionIndex += 1
+        state.currentQuestionIndex += 1;
       }
     },
 
@@ -80,8 +124,7 @@ export const quiz = createSlice({
      * This action does not require a payload.
      */
     restart: () => {
-      return initialState
-    }
-
-  }
-})
+      return initialState;
+    },
+  },
+});
